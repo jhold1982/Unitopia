508782fd9a78bb775a0734d0f1a6c4790d071c27
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-30 15:39:15 -0500

    Initial Commit

---
+++ Unitopia/UnitopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitopiaApp.swift
+//  Unitopia
+//
+//  Created by Justin Hold on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
